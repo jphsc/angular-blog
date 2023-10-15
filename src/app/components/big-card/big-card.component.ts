4e337b92fba0dbd502253bf2f674374b129b80b4
Henrique Costa <rafahsc@live.com>
2023-10-15 15:59:55 -0300

    components dynamics

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
+
+  @Input()
+  photoCover: string = "https://escolasempre.com.br/wp-content/uploads/2023/03/placeholder.png";
+  cardTitle: string = "Saiu a nova versão do Angular";
+  @Input()
+  cardDescription: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis ad dignissimos, reiciendis modi voluptatem qui nulla autem porro velit vero unde similique! Ratione dolor recusandae excepturi. Odio, ipsum explicabo.";
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent {
+  photoCover: string = "https://escolasempre.com.br/wp-content/uploads/2023/03/placeholder.png";
+  cardTitle: string = "Saiu a nova versão do Angular";
 
 }
